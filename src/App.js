563bc825502f700c2a1d0dd36480e7cfe7252568
mohalenezi <ot.m.alenezi@gmail.com>
2021-06-07 20:14:41 +0300

    perfumes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-
+import perfumes from "./products.js";
 function App() {
+  const perfumesList = perfumes.map((perfume) =>
+    (
+      <div className="perfume" key={perfume.id}>
+      <img className="perfume-image" src= {perfume.Image} />
+      <p className="App">{perfume.name}</p>
+      <p className="App">{perfume.Price} KD</p>
+      </div>
+    )
+  
+  )
+
+
+
+
+
+
   return (
+  <div>
     <div className="App">
       <h1 className="App-header"> The Art that makes memory speaks </h1>
        <h3> The best compliment for a good look is a great "SCENT" </h3>
-       <img src="https://media.istockphoto.com/photos/spraying-perfume-on-dark-background-closeup-image-picture-id1155893537?k=6&m=1155893537&s=170667a&w=0&h=6rID0_7RWQbsEwWvQlMkQinwL0ev0afQfdK1AfNqun0=" />
+       <img
+       id="shop-image"
+       src="https://media.istockphoto.com/photos/spraying-perfume-on-dark-background-closeup-image-picture-id1155893537?k=6&m=1155893537&s=170667a&w=0&h=6rID0_7RWQbsEwWvQlMkQinwL0ev0afQfdK1AfNqun0=" />
+       
     </div>
+    <div className="list"> {perfumesList}</div>
+  </div> 
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,25 @@
+const perfumes =[
+    {
+        id: 1,
+        name: "Blue Chanel",
+        Price: 30,
+        Image: 
+        "https://www.chanel.com/images//t_one/t_fragrance//q_auto:good,f_jpg,fl_lossy,dpr_1.2/w_1920/bleu-de-chanel-parfum-spray-3-4fl-oz--packshot-default-107180-8841593094174.jpg"
+    },
+    {
+        id: 2,
+        name: "Montblanc Explorer",
+        Price: 20,
+        Image: 
+        "https://www.montblanc.com/variants/images/19971654707299811/A/w2500.jpg"
+    },
+    {
+        id: 3,
+        name: "Jean paul gaultier Ultra Male",
+        Price: 20,
+        Image: 
+        "https://www.dressinn.com/f/13759/137599411/jean-paul-gaultier-ultra-male-200ml.jpg"
+    }
+]
+
+export default perfumes;
